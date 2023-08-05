2e668270bc6cc8ca931f3731619996b82ad8b1f0
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-08-06 01:47:23 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configreStore';
+import './index.css';
+import App from './App';
 
-function stocklist() {
-  return (
-    <div>
-      <h1>stock Here</h1>
-    </div>
-  );
-}
-
-export default stocklist;
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
