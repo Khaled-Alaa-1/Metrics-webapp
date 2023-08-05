96f2e8a9ff56853a343041b3c21bf5ea6d2b3a7d
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-08-05 03:33:50 +0200

    Fix linters

--- src/redux/cardSlice.js
+++ src/redux/cardSlice.js
@@ -1,38 +1,6 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const fetchCards = createAsyncThunk('cards/fetchCards', async () => {
-  const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=500');
+  const response = await axios.get('https://pokeapi.co/api//v3/covid-19/countries?limit=500');
   return response.data.results;
 });
-
-const cardSlice = createSlice({
-  name: 'cards',
-  initialState: {
-    cards: [],
-    loading: false,
-    error: null,
-  },
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchCards.pending, (state) => {
-        state.loading = true;
-        state.error = null;
-      })
-      .addCase(fetchCards.fulfilled, (state, action) => {
-        state.loading = false;
-        state.cards = action.payload;
-      })
-      .addCase(fetchCards.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.error.message;
-      });
-  },
-});
-
-export const selectCards = (state) => state.cards.cards;
-export const selectLoading = (state) => state.cards.loading;
-export const selectError = (state) => state.cards.error;
-
-export default cardSlice.reducer;
