16288f6b056d655aa7255799e9050e58e0044c85
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-08-07 17:13:34 +0200

    Final touches

--- src/index.js
+++ src/index.js
@@ -6,11 +6,7 @@ import store from './redux/configreStore';
 import './index.css';
 import App from './App';
 
-// Use createRoot instead of ReactDOM.render
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
@@ -18,4 +14,5 @@ root.render(
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
+  document.getElementById('root'),
 );
