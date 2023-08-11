7788ceb2eb5d37c7496a20b7d743d2356cf88df5
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-08-11 02:15:28 +0200

    Fix Linters

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import store from './redux/configreStore';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+// Use createRoot() instead of ReactDOM.render()
+const root = ReactDOM.createRoot(rootElement);
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
@@ -14,5 +17,4 @@ ReactDOM.render(
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
